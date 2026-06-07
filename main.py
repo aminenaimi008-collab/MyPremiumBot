8fb0667c7bd49bfd7086440871b2a50e70340efe
aminenaimi008-collab <aminenaimi008@gmail.com>
2026-06-07 17:57:28 +0100

    Fix error message, add start.sh

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ bot = Bot()
 
 if __name__ == "__main__":
     if not config.DISCORD_TOKEN:
-        print("Error: DISCORD_TOKEN not set in .env file")
+        print("Error: DISCORD_TOKEN not set. Create .env file or set environment variable DISCORD_TOKEN")
         exit(1)
     asyncio.run(bot.start(config.DISCORD_TOKEN))
